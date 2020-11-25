dad64d4ab1767e6eb24f0903f21d09eebcd4f08d
Renato Mateus <renatomateusx@gmail.com>
2020-11-25 18:41:20 -0300

    implementação do config server

--- src/main/java/com/renatomateusx/configserver/ConfigServerApplication.java
+++ src/main/java/com/renatomateusx/configserver/ConfigServerApplication.java
@@ -3,13 +3,14 @@ package com.renatomateusx.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
